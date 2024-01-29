6ab1f53a48e98638831df5a3b7cc33d298ae358f
igaryo0506 <igarashi-ryo0506@g.ecc.u-tokyo.ac.jp>
2024-01-29 16:51:31 +0900

    feature: make View

--- ContentView.swift
+++ ContentView.swift
@@ -1,21 +1,102 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var board: [[Brick]] = Array(repeating: Array(repeating: .init(color: .black), count: 8), count: 8) + Array(repeating: Array(repeating: .init(), count: 8), count: 8)
+    var body: some View {
+        GameView()
+    }
+}
+
+struct GameView: View {
+    @ObservedObject var gameViewModel: GameViewModel = .init()
     var body: some View {
         ZStack {
-            Grid(horizontalSpacing: 1, verticalSpacing: 1) {
-                ForEach(0 ..< board.count, id: \.self) { lineIndex in
-                    GridRow {
-                        ForEach(0 ..< board[lineIndex].count, id: \.self) { pixelIndex in
-                            BrickView(brick: $board[lineIndex][pixelIndex])
+            VStack {
+                Spacer(minLength: 100)
+                GeometryReader { proxy in
+                    Grid(horizontalSpacing: 1, verticalSpacing: 1) {
+                        ForEach(0 ..< gameViewModel.bricks.count, id: \.self) { lineIndex in
+                            GridRow {
+                                ForEach(0 ..< gameViewModel.bricks[lineIndex].count, id: \.self) { pixelIndex in
+                                    BrickView(brick: $gameViewModel.bricks[lineIndex][pixelIndex])
+                                }
+                            }
                         }
                     }
+                    .onAppear{
+                        gameViewModel.prepare(boardFrame: proxy.frame(in: .global))
+                        gameViewModel.start()
+                    }
                 }
+                Spacer(minLength: 100)
             }
-            .padding(.vertical, 32)
-            .background(.gray)
+            BallView(ball: $gameViewModel.ball)
+                .position(CGPoint(x: gameViewModel.ball.position.x, y: gameViewModel.ball.position.y))
         }
+        .background(.gray)
+        .ignoresSafeArea()
+    }
+}
+
+class GameViewModel: ObservableObject {
+    let xLength = 8
+    let yLength = 16
+    @Published var bricks: [[Brick]]
+    @Published var ball: Ball
+    private var boardFrame: CGRect = .zero
+    var width: CGFloat {
+        boardFrame.width
+    }
+    var height: CGFloat {
+        boardFrame.height
+    }
+    
+    init() {
+        self.bricks = Array(repeating: Array(repeating: .init(color: .black), count: xLength), count: yLength / 2) + Array(repeating: Array(repeating: .init(), count: xLength), count: yLength / 2)
+        self.ball = .init(position: .zero, color: .blue)
+    }
+    
+    func prepare(boardFrame: CGRect) {
+        self.ball.position = .init(x: boardFrame.midX, y: boardFrame.minY + 10)
+        self.boardFrame = boardFrame
+    }
+    
+    func start() {
+        var timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { [weak self] _ in
+            guard let self else { return }
+            self.ball.update()
+            checkConflict()
+        }
+    }
+    
+    func checkConflict() {
+        if boardFrame.minX > ball.position.x {
+            ball.toggleDirectionX()
+        }
+        if boardFrame.maxX < ball.position.x {
+            ball.toggleDirectionX()
+        }
+        if boardFrame.minY > ball.position.y {
+            ball.toggleDirectionY()
+        }
+        if boardFrame.maxY < ball.position.y {
+            ball.toggleDirectionY()
+        }
+        var xIndex = Int((ball.position.x - boardFrame.minX) / (width / CGFloat(xLength)))
+        var yIndex = Int((ball.position.y - boardFrame.minY) / (height / CGFloat(yLength)))
+        if 0 <= xIndex && xIndex < xLength && 0 <= yIndex && yIndex < yLength {
+            bricks[yIndex][xIndex].toggle()
+        }
+        print(xIndex, yIndex)
+    }
+}
+
+
+struct BallView: View {
+    @Binding var ball: Ball
+    var body: some View {
+        Circle()
+            .frame(width: 16, height: 16)
+            .foregroundStyle(.blue)
     }
 }
 
@@ -30,6 +111,28 @@ struct BrickView: View {
     }
 }
 
+struct Ball {
+    var id: UUID = .init()
+    var position: CGPoint
+    var color: Color
+    var direction: CGVector
+    init(position: CGPoint, color: Color) {
+        self.position = position
+        self.color = color
+        self.direction = .init(dx: 10, dy: 5)
+    }
+    mutating func update() {
+        position = CGPoint(x: position.x + direction.dx, y: position.y + direction.dy)
+    }
+    
+    mutating func toggleDirectionX(){
+        direction.dx = -direction.dx
+    }
+    
+    mutating func toggleDirectionY(){
+        direction.dy = -direction.dy
+    }
+}
 
 struct Brick {
     var id: UUID = .init()
